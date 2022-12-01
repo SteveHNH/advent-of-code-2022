518010364285df5eb8674f5b46d4b9e551141cf5
Stephen Adams <stephen.adams@redhat.com>
2022-12-01 10:50:05 -0500

    Day 1
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

---
+++ 1/puzzle_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+# Advent of Code - Day 1
+
+def get_lines(path):
+    f = open(path, "r")
+    lines = f.readlines()
+    return lines
+
+
+def _sum(arr):
+    sum = 0
+
+    for i in arr:
+        sum = sum + i
+
+    return sum
+
+
+def main():
+    values = []
+    totals = []
+    arr = []
+
+    lines = get_lines("./input.txt")
+
+    for line in lines:
+        if line.strip() != "":
+            arr.append(int(line))
+        else:
+            values.append(arr)
+            arr = []
+
+    for _set in values:
+        totals.append(_sum(_set))
+
+    sorted_totals = sorted(totals)
+    # part 1
+    print("Top Calories: " + str(sorted_totals[-1]))
+    # part 2
+    print("Top 3 Calories: " + str(sum(sorted_totals[-3:])))
+
+
+if __name__ == "__main__":
+    main()
